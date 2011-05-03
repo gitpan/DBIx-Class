51f6caed01db6e327b05828abe752842b36221c8
Arthur Axel "fREW" Schmidt <frioux+cpan@gmail.com>
2011-05-03 00:50:08 +0000

    Import of FREW/DBIx-Class-0.08191 from CPAN.
    gitpan-cpan-distribution: DBIx-Class
    gitpan-cpan-version:      0.08191
    gitpan-cpan-path:         FREW/DBIx-Class-0.08191.tar.gz
    gitpan-cpan-author:       FREW
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class.pm
+++ lib/DBIx/Class.pm
@@ -69,7 +69,7 @@ sub component_base_class { 'DBIx::Class' }
 # Always remember to do all digits for the version even if they're 0
 # i.e. first release of 0.XX *must* be 0.XX000. This avoids fBSD ports
 # brain damage and presumably various other packaging systems too
-$VERSION = '0.08190_03';
+$VERSION = '0.08191';
 
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
 
@@ -355,6 +355,8 @@ hobbs: Andrew Rodland <arodland@cpan.org>
 ilmari: Dagfinn Ilmari MannsE<aring>ker <ilmari@ilmari.org>
 
 initself: Mike Baas <mike@initselftech.com>
+
+ironcamel: Naveed Massjouni <naveedm9@gmail.com>
 
 jawnsy: Jonathan Yu <jawnsy@cpan.org>
 
