667a070f64ee9c23a133553310293d1f09e27c1c
Andrew Rodland <ARODLAND@cpan.org>
2011-11-29 05:42:54 +0000

    Import of ARODLAND/DBIx-Class-0.08196 from CPAN.
    gitpan-cpan-distribution: DBIx-Class
    gitpan-cpan-version:      0.08196
    gitpan-cpan-path:         ARODLAND/DBIx-Class-0.08196.tar.gz
    gitpan-cpan-author:       ARODLAND
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class.pm
+++ lib/DBIx/Class.pm
@@ -11,7 +11,7 @@ our $VERSION;
 # $VERSION declaration must stay up here, ahead of any other package
 # declarations, as to not confuse various modules attempting to determine
 # this ones version, whether that be s.c.o. or Module::Metadata, etc
-$VERSION = '0.08195_02';
+$VERSION = '0.08196';
 
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
 
@@ -424,7 +424,7 @@ Numa: Dan Sully <daniel@cpan.org>
 
 ovid: Curtis "Ovid" Poe <ovid@cpan.org>
 
-oyse: Øystein Torget <oystein.torget@dnv.com>
+oyse: E<Oslash>ystein Torget <oystein.torget@dnv.com>
 
 paulm: Paul Makepeace
 
@@ -433,6 +433,8 @@ penguin: K J Cheetham
 perigrin: Chris Prather <chris@prather.org>
 
 peter: Peter Collingbourne <peter@pcc.me.uk>
+
+Peter Valdemar ME<oslash>rch <peter@morch.com>
 
 phaylon: Robert Sedlacek <phaylon@dunkelheit.at>
 
