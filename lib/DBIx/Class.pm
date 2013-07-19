c0549f282a870948d561181b428806f19646e0a7
Arthur Axel "fREW" Schmidt <frioux+cpan@gmail.com>
2013-07-19 20:29:25 +0000

    Import of FREW/DBIx-Class-0.08250_02 from CPAN.
    gitpan-cpan-distribution: DBIx-Class
    gitpan-cpan-version:      0.08250_02
    gitpan-cpan-path:         FREW/DBIx-Class-0.08250_02.tar.gz
    gitpan-cpan-author:       FREW
    gitpan-cpan-maturity:     developer

--- lib/DBIx/Class.pm
+++ lib/DBIx/Class.pm
@@ -11,7 +11,7 @@ our $VERSION;
 # $VERSION declaration must stay up here, ahead of any other package
 # declarations, as to not confuse various modules attempting to determine
 # this ones version, whether that be s.c.o. or Module::Metadata, etc
-$VERSION = '0.08250_01';
+$VERSION = '0.08250_02';
 
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
 
@@ -136,6 +136,11 @@ list below is sorted by "fastest response time":
 =back
 
 =head1 SYNOPSIS
+
+For the very impatient: L<DBIx::Class::Manual::QuickStart>
+
+This code in the next step can be generated automatically from an existing
+database, see L<dbicdump> from the distribution C<DBIx-Class-Schema-Loader>.
 
 =head2 Schema classes preparation
 
